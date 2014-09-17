5e429f0b601805ff15620589a130cfaaea136031
Nikita Lyubchich <cybran111@gmail.com>
2014-09-17 23:35:06 +0300

    Added setup.py, license, and some other changes

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from distutils.core import setup
 setup(
-    name = ''
+    name = 'middlecoords',
+    version = '1.0.0',
+    description='Function which calculates coordinates between given positions',
+    author = "Nikita Lyubchich",
+    author_email='nikita.lyubchich@gmail.com',
+    url = "https://github.com/Cybran111/MiddleCoords",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent"
+    ]
 )
--- test_middlecoords.py
+++ test_middlecoords.py
@@ -1,20 +1,19 @@
 import unittest
-import middleposition
+import middlecoords
 
 
 class MiddlePositionTests(unittest.TestCase):
-
     def setUp(self):
         """
         Set up our calculator, default position and expected result
         """
 
-        self.calculator = middleposition.get_middle_position
+        self.calculator = middlecoords.get_middle_position
 
         self.first_lat, self.first_long = 50.5000, 30.8000
         self.second_lat, self.second_long = 50.7000, 31.0000
         self.third_lat, self.third_long = 50.7000, 30.8000
-        self.fourth_lat, self.fourth_long= 50.5000, 31.0000
+        self.fourth_lat, self.fourth_long = 50.5000, 31.0000
 
         self.middle_position = 50.6000, 30.9000
 
@@ -23,7 +22,7 @@ class MiddlePositionTests(unittest.TestCase):
         get_middle_position() should get two positions and calculate the middle position
         """
         result_position = self.calculator(self.first_lat, self.first_long,
-                                          self.second_lat, self.second_long,)
+                                          self.second_lat, self.second_long, )
 
         self.assertEqual(result_position, self.middle_position)
 
@@ -32,21 +31,19 @@ class MiddlePositionTests(unittest.TestCase):
         get_middle_position() should get positions (no matter how many: 1,2, 4, 256) and calculate middle position
         """
         result_position = self.calculator(self.first_lat, self.first_long,
-                                            self.second_lat, self.second_long,
-                                            self.third_lat, self.third_long,
-                                            self.fourth_lat, self.fourth_long)
+                                          self.second_lat, self.second_long,
+                                          self.third_lat, self.third_long,
+                                          self.fourth_lat, self.fourth_long)
         self.assertEqual(result_position, self.middle_position)
 
     def test_unpaired_position(self):
         """
         get_middle_position() should raise exception if not paired values passed
         """
-        self.assertRaises(middleposition.NoPair,
-                            self.calculator,
-                                self.first_lat, self.first_long,
-                                self.second_lat)
-
-
+        self.assertRaises(middlecoords.NoPair,
+                          self.calculator,
+                          self.first_lat, self.first_long,
+                          self.second_lat)
 
 
 if __name__ == '__main__':
